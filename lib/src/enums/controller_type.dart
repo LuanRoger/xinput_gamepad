40a3b7ec0ca8ff0c79cc2add18615f0beed8b037
LuanRoger <luan.roger.2003@gmail.com>
2022-12-16 10:53:47 -0300

    Fix unknow state at initialization

--- lib/src/enums/controller_type.dart
+++ lib/src/enums/controller_type.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: constant_identifier_names
 
 import 'package:win32/win32.dart';
+import 'package:collection/collection.dart';
 
 ///The type of the controller.
 enum ControllerType {
+  UNKNOWN(0),
   GAMEPAD(XINPUT_DEVTYPE_GAMEPAD);
 
   final int controllerTypeBitmask;
@@ -11,5 +13,5 @@ enum ControllerType {
   const ControllerType(this.controllerTypeBitmask);
 
   factory ControllerType.fromBitmask(int bitmask) => ControllerType.values
-      .firstWhere((element) => element.controllerTypeBitmask == bitmask);
+      .firstWhereOrNull((element) => element.controllerTypeBitmask == bitmask) ?? ControllerType.UNKNOWN;
 }
